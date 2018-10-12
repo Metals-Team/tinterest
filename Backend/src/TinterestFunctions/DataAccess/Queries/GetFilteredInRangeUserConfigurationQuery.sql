35395d30769b84c6b60be964d88bd578158f8a5f
MadeByJK <madebyjk@hotmail.com>
2018-10-12 17:28:24 +0200

    In range filter

--- Backend/src/TinterestFunctions/DataAccess/Queries/GetFilteredInRangeUserConfigurationQuery.sql
+++ Backend/src/TinterestFunctions/DataAccess/Queries/GetFilteredInRangeUserConfigurationQuery.sql
@@ -1,4 +1,9 @@
 SELECT dbo.UserConfiguration.*
 FROM dbo.UserConfiguration
-LEFT JOIN dbo.UserConfiguration_Interests
-WHERE InterestId = @InterestId AND 
+LEFT JOIN dbo.UserConfiguration_Interests ON dbo.UserConfiguration.Id = dbo.UserConfiguration_Interests.UserId
+WHERE
+InterestId = @InterestId AND
+@MinLatitude < Latitude AND
+@MaxLatitude > Latitude AND
+@MinLongitude < Longitude AND
+@MaxLongitude > Longitude
--- Backend/src/TinterestFunctions/UserFunctions/UsersInRangeFunction.cs
+++ Backend/src/TinterestFunctions/UserFunctions/UsersInRangeFunction.cs
@@ -1,26 +1,23 @@
-
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using MetalsTeam.Tinterest.UserFunctions.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace UserFunctions
 {
-    public static class UsersInRangeFunction
-    {
-        [FunctionName("UsersInRangeFunction")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
+	public static class UsersInRangeFunction
+	{
+		[FunctionName("UsersInRangeFunction")]
+		public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log, ExecutionContext context)
 		{
-			using (var reader = new StreamReader(this.request.Body))
-			{
-				return JsonConvert.DeserializeObject<RangeFilter>(await reader.ReadToEndAsync());			}
-
+			return await new UsersInRangeService()
+				.ForRequest(req)
+				.WithExecutionContext(context)
+				.WithLogger(log)
+				.Run();
 		}
-    }
+	}
 }
